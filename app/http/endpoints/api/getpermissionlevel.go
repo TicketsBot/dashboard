ba7e6f02f52bb03d383ef57377622ef6ab062c8d
rxdn <dotrar@protonmail.com>
2020-07-06 17:40:30 +0100

    guild count check

--- app/http/endpoints/api/getpermissionlevel.go
+++ app/http/endpoints/api/getpermissionlevel.go
@@ -12,9 +12,20 @@ import (
 func GetPermissionLevel(ctx *gin.Context) {
 	userId := ctx.Keys["userid"].(uint64)
 
+	guilds := strings.Split(ctx.Query("guilds"), ",")
+	if len(guilds) > 100 {
+		ctx.JSON(400, gin.H{
+			"success": false,
+			"error": "too many guilds",
+		})
+		return
+	}
+
+	// TODO: Check whether the bot is in the guild to prevent us getting maliciously 429'd
+
 	levels := make(map[string]permission.PermissionLevel)
 
-	for _, raw := range strings.Split(ctx.Query("guilds"), ",") {
+	for _, raw := range guilds {
 		guildId, err := strconv.ParseUint(raw, 10, 64)
 		if err != nil {
 			ctx.JSON(400, gin.H{
@@ -27,7 +38,6 @@ func GetPermissionLevel(ctx *gin.Context) {
 		level := utils.GetPermissionLevel(guildId, userId)
 		levels[strconv.FormatUint(guildId, 10)] = level
 	}
-
 
 	ctx.JSON(200, gin.H{
 		"success": true,
