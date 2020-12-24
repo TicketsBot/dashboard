e4433d4eda30cc6467db4dad413a993c2577b824
rxdn <29165304+rxdn@users.noreply.github.com>
2020-12-24 23:27:30 +0000

    interaction creation cooldown

--- app/http/endpoints/api/whitelabel/whitelabelcreateinteractions.go
+++ app/http/endpoints/api/whitelabel/whitelabelcreateinteractions.go
@@ -1,8 +1,10 @@
 package api
 
 import (
+	"fmt"
 	"github.com/TicketsBot/GoPanel/botcontext"
 	"github.com/TicketsBot/GoPanel/database"
+	"github.com/TicketsBot/GoPanel/messagequeue"
 	command "github.com/TicketsBot/worker/bot/command/impl"
 	"github.com/gin-gonic/gin"
 	"github.com/rxdn/gdl/rest"
@@ -11,8 +13,6 @@ import (
 
 func WhitelabelCreateInteractions(ctx *gin.Context) {
 	userId := ctx.Keys["userid"].(uint64)
-
-	//TODO: COOLDOWN
 
 	// Get bot
 	bot, err := database.Client.Whitelabel.GetByUserId(userId)
@@ -30,6 +30,38 @@ func WhitelabelCreateInteractions(ctx *gin.Context) {
 			"success": false,
 			"error":   "No bot found",
 		})
+		return
+	}
+
+	// Cooldown
+	key := fmt.Sprintf("tickets:interaction-create-cooldown:%d", bot.BotId)
+
+	// try to set first, prevent race condition
+	wasSet, err := messagequeue.Client.SetNX(key, 1, time.Minute * 15).Result()
+	if err != nil {
+		ctx.JSON(500, gin.H{
+			"success": false,
+			"error": err.Error(),
+		})
+		return
+	}
+
+	// on cooldown, tell user how long left
+	if !wasSet {
+		expiration, err := messagequeue.Client.TTL(key).Result()
+		if err != nil {
+			ctx.JSON(500, gin.H{
+				"success": false,
+				"error": err.Error(),
+			})
+			return
+		}
+
+		ctx.JSON(400, gin.H{
+			"success": false,
+			"error": fmt.Sprintf("Interaction creation on cooldown, please wait another %f minutes", expiration.Minutes()),
+		})
+
 		return
 	}
 
