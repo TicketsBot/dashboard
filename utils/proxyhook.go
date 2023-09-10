d1997e1e2aebbc3f28b1eb3ff4fd381b6142f533
rxdn <29165304+rxdn@users.noreply.github.com>
2023-09-10 17:51:17 +0100

    Proxy whitelabel / bearer requests

--- utils/proxyhook.go
+++ utils/proxyhook.go
@@ -3,14 +3,11 @@ package utils
 import (
 	"github.com/TicketsBot/GoPanel/config"
 	"net/http"
-	"os"
 )
 
 // Twilight's HTTP proxy doesn't support the typical HTTP proxy protocol - instead you send the request directly
 // to the proxy's host in the URL. This is not how Go's proxy function should be used, but it works :)
 func ProxyHook(token string, req *http.Request) {
-	if token == os.Getenv("WORKER_PUBLIC_TOKEN") {
-		req.URL.Scheme = "http"
-		req.URL.Host = config.Conf.Bot.ProxyUrl
-	}
+	req.URL.Scheme = "http"
+	req.URL.Host = config.Conf.Bot.ProxyUrl
 }
