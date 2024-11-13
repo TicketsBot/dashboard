7244b536221bb7fcdd866dfc69ec5ef1924bf543
rxdn <29165304+rxdn@users.noreply.github.com>
2024-11-13 23:08:38 +0000

    Update proxy hook

--- utils/proxyhook.go
+++ utils/proxyhook.go
@@ -3,11 +3,14 @@ package utils
 import (
 	"github.com/TicketsBot/GoPanel/config"
 	"net/http"
+	"strings"
 )
 
 // Twilight's HTTP proxy doesn't support the typical HTTP proxy protocol - instead you send the request directly
 // to the proxy's host in the URL. This is not how Go's proxy function should be used, but it works :)
 func ProxyHook(token string, req *http.Request) {
-	req.URL.Scheme = "http"
-	req.URL.Host = config.Conf.Bot.ProxyUrl
+	if !strings.HasPrefix(req.Header.Get("Authorization"), "Basic") {
+		req.URL.Scheme = "http"
+		req.URL.Host = config.Conf.Bot.ProxyUrl
+	}
 }
