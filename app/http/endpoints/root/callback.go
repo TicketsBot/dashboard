07943c2e1a9b8788d030b4b19db3e656513b6481
Dan <damuffin36@gmail.com>
2025-02-16 05:25:47 -0800

    Fix: Dashboard login loop (#37)

--- app/http/endpoints/root/callback.go
+++ app/http/endpoints/root/callback.go
@@ -4,6 +4,11 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+
 	"github.com/TicketsBot/GoPanel/app"
 	"github.com/TicketsBot/GoPanel/app/http/session"
 	"github.com/TicketsBot/GoPanel/config"
@@ -12,10 +17,6 @@ import (
 	"github.com/golang-jwt/jwt"
 	"github.com/rxdn/gdl/rest"
 	"github.com/rxdn/gdl/rest/request"
-	"net/http"
-	"strconv"
-	"strings"
-	"time"
 )
 
 func CallbackHandler(c *gin.Context) {
@@ -61,7 +62,7 @@ func CallbackHandler(c *gin.Context) {
 		HasGuilds:    false,
 	}
 
-	var guilds []utils.GuildDto
+	guilds := make([]utils.GuildDto, 0)
 	if utils.Contains(scopes, "guilds") {
 		guilds, err = utils.LoadGuilds(c, res.AccessToken, currentUser.Id)
 		if err != nil {
@@ -98,9 +99,7 @@ func CallbackHandler(c *gin.Context) {
 			"avatar":   currentUser.Avatar,
 			"admin":    utils.Contains(config.Conf.Admins, currentUser.Id),
 		},
-	}
-	if len(guilds) > 0 {
-		resMap["guilds"] = guilds
+		"guilds": guilds,
 	}
 
 	c.JSON(http.StatusOK, resMap)
